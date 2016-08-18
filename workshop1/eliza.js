adaa030c84f75452183c15c770e914d68e05da1e
jg4103 <jg4103@bard.edu>
2016-08-18 12:23:40 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -28,8 +28,8 @@
  */
 
 var rules = {
-  "~*x hello ~*y": [
-    "How do you do. Please state your problem."
+  "~*x spory ~*y": [
+    "My favorite sport is soccer."
   ],
   "~*x computer ~*y": [
     "Do computers worry you?",
